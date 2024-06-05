9c4802e1d04db07593dc83044c1c38d5e589aaf3
David Molina <davidmolflo@gmail.com>
2024-06-05 18:42:33 -0500

    Clase 05 Junio_1

---
+++ js/api.js
@@ -0,0 +1,31 @@
+const apiUrl = "https://pokeapi.co/api/v2/";
+function consutar(detalle){
+fetch(apiUrl + detalle)
+  .then((response) => {
+    // Verificar si la respuesta es exitosa
+    if (!response.ok) {
+      throw new Error("Error en la solicitud: " + response.statusText);
+    }
+    // Convertir la respuesta a JSON
+    return response.json();
+  })
+  .then((data) => {
+    // Aquí puedes hacer algo con los datos recibidos
+    console.log(data);
+    var body = document.getElementById("content");
+    body.innerHTML = data.name;
+  })
+  .catch((error) => {
+    // Manejo de errores
+    console.error("Hubo un problema con la solicitud Fetch:", error);
+  });
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+    const boton = document.getElementById("consultar");
+    boton.addEventListener("click",()=>{
+        const campo = document.getElementById("detalle");
+        const detalle = campo.value;
+        consutar(detalle);
+    });
+})
